587993486ec4a6525d28799d726b14ed1fe8d57f
pksiitk20 <princek@apporchid.com>
2023-03-15 19:07:08 +0530

    Initial commit "Basic Docker"

---
+++ learner_project/app/app.py
@@ -0,0 +1,12 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello_world():
+    return 'Hello World! How are you doing?'
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=8008, debug=True)
